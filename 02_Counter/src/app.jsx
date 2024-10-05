52c3ab33b3e35b6bd6da76a0c7c47033a3ae948c
MamtaRajera <mamtarajera157@gmaill.com>
2024-10-05 20:38:42 +0530

    container config done

---
+++ 02_Counter/src/app.jsx
@@ -0,0 +1,37 @@
+
+import {useState} from 'react'
+
+function App() {
+  
+
+    //let counter = 5;
+    let [counter, setCounter] =  useState(5);
+
+  const addValue = () => {
+    //counter = counter + 1;
+    setCounter(counter+1)
+    //setCounter(counter+1)
+    console.log(counter)
+  }
+
+  const removeValue = () => {
+    setCounter(counter-1)
+    console.log(counter)
+  }
+
+  return (
+    <>
+       <h1>Counter Value: {counter}</h1>
+
+       <button onClick = {addValue}>Add Value {counter}</button>
+       <br />
+       <button onClick = {removeValue}>Remove Value {counter}</button>
+
+       <footer>{counter}</footer>
+    </>
+  )
+}
+
+
+export default App
+
